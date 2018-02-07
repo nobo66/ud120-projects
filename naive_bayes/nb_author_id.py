ae869b15c514a26baabb911ce9d79740c7c29828
Noboru Murakami <noboru.murakami@gmail.com>
2018-02-08 02:24:21 +0900

    update for #43 Timing Your NB Classifier.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,9 +29,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
 
+t0 = time()
 predict = clf.predict(features_test)
+print "predict time:", round(time() - t0, 3), "s"
 
 # accuracy
 from sklearn.metrics import accuracy_score
