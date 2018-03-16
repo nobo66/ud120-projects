49dac4b140acbbdf26f71598ae59073d092b4940
Noboru Murakami <noboru.murakami@gmail.com>
2018-03-17 06:17:48 +0900

    updated for lesson3-30,31.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+import numpy as np
+from sklearn.svm import SVC
+clf = SVC(kernel='linear')
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
 
+t0 = time()
+predict = clf.predict(features_test)
+print "predict time:", round(time() - t0, 3), "s"
+
+# accuracy
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, predict)
+print("accuracy is {0}".format(accuracy))
 #########################################################
 
 
