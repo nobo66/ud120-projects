46aaa53b8634b1437862322ee1b7e64c67291cc9
Noboru Murakami <noboru.murakami@gmail.com>
2018-03-22 08:16:56 +0900

    updated for lesson4-39,40,41.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -26,6 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 print("number of features is {0}".format(len(features_train[0])))
 
+from sklearn import tree
+# added min_samples_split setting referring below site.
+# https://jefflirion.github.io/udacity/Intro_to_Machine_Learning/Lesson3.html
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+predicts = clf.predict(features_test)
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, predicts)
+#accuracy = accuracy_score(predicts, labels_test)
+print("accuracy is {0}".format(accuracy))
 #########################################################
 
 
