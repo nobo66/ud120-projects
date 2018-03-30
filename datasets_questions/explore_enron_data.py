6edce58726dcd7ea63165d58c2915ef69d54b14b
Noboru Murakami <noboru.murakami@gmail.com>
2018-03-31 07:47:08 +0900

    updating for lesson6.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,19 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-
-
+print enron_data['METTS MARK']
+print("number of data points is {0}".format(len(enron_data)))
+print("number of features to each person is {0}".format(len(enron_data['METTS MARK'])))
+num_of_POI = sum(1 for x in enron_data.values() if x['poi'] == 1)
+num_of_POI = sum(x['poi'] == 1 for x in enron_data.values())
+print("number of POI is {0}".format(num_of_POI))
+print("number of POI is {0}".format(sum(1 for x in enron_data.values() if x['poi'] == 1)))
+print("number of POI is {0}".format(sum(x['poi'] == 1 for x in enron_data.values())))
+#print for x in enron_data.values()
+print("the total value of the stock belonging to James Prentice is {0}".format(enron_data['PRENTICE JAMES']['total_stock_value']))
+print("How many email messages do we have from Wesley Colwell to persons of interest?"\
+	"\nAnswer:{0}".format(enron_data['COLWELL WESLEY']['from_this_person_to_poi']))
+print("What's the value of stock options exercised by Jeffrey K Skilling?"
+	"\nAnswer:{0}".format(enron_data['SKILLING JEFFREY K']['exercised_stock_options']))
+#print(""\
+#	"\nAnswer:{0}".format(enron_data['']['']))
