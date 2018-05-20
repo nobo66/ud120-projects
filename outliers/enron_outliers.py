790588fa1ca9555397953838ab90f3383a5c117f
Noboru Murakami <noboru.murakami@gmail.com>
2018-05-21 06:00:25 +0900

    updated for Lesson8-16

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,6 +10,8 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+# Lesson8-17
+data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
 
