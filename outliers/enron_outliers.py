9fd3b5e46585117b2253c0c93030ebe5eb19f98d
Noboru Murakami <noboru.murakami@gmail.com>
2018-05-22 06:10:40 +0900

    updated for Lesson8-18.

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -14,6 +14,13 @@ features = ["salary", "bonus"]
 data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
+# Lesson8-18 Check outliers
+for key, personal_data in data_dict.iteritems():
+	if personal_data['salary'] >= 1000000 and personal_data['salary'] != "NaN" and \
+	personal_data['bonus'] >= 5000000 and personal_data['bonus'] != "NaN":
+		print("{0} is a person whose salary is over 1,000,000USD and bonus is over 5,000,000USD".format(key))
+		print("data={0}".format(personal_data))
+
 
 ### your code below
 for point in data:
