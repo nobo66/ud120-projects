d299f5cae223ea0725ac2dc35431380b3eb09a98
Noboru <noboru.murakami@gmail.com>
2018-10-13 08:50:02 +0900

    updated for Lesson14-18.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -35,3 +35,10 @@ clf = clf.fit(features, labels)
 score = clf.score(features, labels)
 print "score is {0}".format(score)
 
+# Lesson14-18
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+        features, labels, test_size=0.3, random_state=42)
+clf = clf.fit(features_train, labels_train)
+score = clf.score(features_test, labels_test)
+print "The score of test data is {0}".format(score)
