d5f1cdfaa29dc9ca6c5b0b2e1d878c35834325ed
Noboru <noboru.murakami@gmail.com>
2019-07-15 07:25:28 +0900

    updated for lesson15-27.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,19 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+# Lesson 15-27
+# from Lesson14-17
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+score = clf.score(features, labels)
+print "score is {0}".format(score)
+
+# from Lesson14-18
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+        features, labels, test_size=0.3, random_state=42)
+clf = clf.fit(features_train, labels_train)
+score = clf.score(features_test, labels_test)
+print "The score of test data is {0}".format(score)
 
